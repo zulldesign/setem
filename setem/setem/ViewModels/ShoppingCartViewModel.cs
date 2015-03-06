6ed19d23d1282ee3a0e16ef9120efa51c903ca48
zulldesign <admin@zulldesign.ml>
2015-03-06 18:33:25 +0800

    l

---
+++ setem/setem/ViewModels/ShoppingCartViewModel.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+using setem.Models;
+
+namespace setem.ViewModels
+{
+    public class ShoppingCartViewModel
+    {
+        public List<Cart> CartItems { get; set; }
+        public decimal CartTotal { get; set; }
+    }
+}
